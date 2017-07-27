3a55bb8a831cda83ca8d6d3007fa9989722ae06d
Mauricio Reyes <mreyes@talpor.com>
2017-07-26 20:43:58 -0400

    add requirements

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# tested on Ubuntu 16.10
+
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+
+sudo apt-get -y install gir1.2-appindicator3-0.1
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-gobject
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install solc
