0081dc07e4d130889f3f56a942e706e593302258
Mauricio Reyes <mreyes@talpor.com>
2018-06-07 13:33:27 -0400

    refactor

--- truffle.js
+++ truffle.js
@@ -5,16 +5,9 @@ require('babel-polyfill')
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    rinkeby: {
-      host: "localhost",
-      from: "0x0",
-      port: 8545,
-      network_id: 4,
-      gas: 4612388
     }
   }
 };
